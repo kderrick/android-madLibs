18119f37c530764a237b71f9b1e19ef97cd4de4d
Josh Gustafson and Kyle Derrick <silversevenfox@yahoo.com>
2016-04-19 10:53:18 -0700

    add homeIntent button to MadLibDisplay Activity

--- app/src/main/java/com/epicodus/madlibs/MadLibDisplayActivity.java
+++ app/src/main/java/com/epicodus/madlibs/MadLibDisplayActivity.java
@@ -3,12 +3,15 @@ package com.epicodus.madlibs;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MadLibDisplayActivity extends AppCompatActivity {
 
     public static final String TAG = MadLibDisplayActivity.class.getSimpleName();
     private TextView mDisplayMadLibsTextView;
+    private Button mHomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,15 @@ public class MadLibDisplayActivity extends AppCompatActivity {
         String madLib = ("Here is our story. One time a "  + noun + " with " +  adjective + " fur" + "something something something" +  verb);
         mDisplayMadLibsTextView.setText(madLib);
 
+        mHomeButton = (Button) findViewById(R.id.homeButton);
+        mHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homeIntent = new Intent(MadLibDisplayActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+            }
+        });
 
-    }
+
+    };
 }
