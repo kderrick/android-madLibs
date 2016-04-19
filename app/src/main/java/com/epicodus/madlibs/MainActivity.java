2b99dd8c16da566f5e7ea78944f4d584cabd6a9c
Josh Gustafson and Kyle Derrick <kderrick1988@gmail.com>
2016-04-19 09:13:45 -0700

    inital commit

---
+++ app/src/main/java/com/epicodus/madlibs/MainActivity.java
@@ -0,0 +1,39 @@
+package com.epicodus.madlibs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private Button mSubmitButton;
+    private EditText mNounEditText;
+    private EditText mAdjectiveEditText;
+    private EditText mVerbEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNounEditText = (EditText) findViewById(R.id.nounEditText);
+        mAdjectiveEditText = (EditText) findViewById(R.id.adjectiveEditText2);
+        mVerbEditText = (EditText) findViewById(R.id.verbEditText3);
+        mSubmitButton = (Button) findViewById(R.id.submitButton);
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String noun = mNounEditText.getText().toString();
+                String adjective = mAdjectiveEditText.getText().toString();
+                String verb = mVerbEditText.getText().toString();
+                Log.d(TAG, noun);
+                Log.d(TAG, adjective);
+                Log.d(TAG, verb);
+
+            }
+        });
+    }
+}
